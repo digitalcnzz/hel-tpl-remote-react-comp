335da0bdcbc686149c607a4df092713cc3213195
li.jw <li.jw@digitalcnzz.com>
2023-06-15 10:16:59 +0800

    feat: 支持less文件

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -189,6 +189,73 @@ module.exports = function (webpackEnv) {
     return loaders;
   };
 
+  const getLessLoaders = (cssOptions, preProcessor) => {
+    const loaders = [
+      isEnvDevelopment && require.resolve("style-loader"),
+      isEnvProduction && {
+        loader: MiniCssExtractPlugin.loader,
+        // css is located in `static/css`, use '../../' to locate index.html folder
+        // in production `paths.publicUrlOrPath` can be a relative path
+        options: paths.publicUrlOrPath.startsWith(".")
+          ? { publicPath: "../../" }
+          : {},
+      },
+      {
+        loader: require.resolve("css-loader"),
+        options: cssOptions,
+      },
+      {
+        // Options for PostCSS as we reference these options twice
+        // Adds vendor prefixing based on your specified browser support in
+        // package.json
+        loader: require.resolve("postcss-loader"),
+        options: {
+          postcssOptions: {
+            // Necessary for external CSS imports to work
+            // https://github.com/facebook/create-react-app/issues/2677
+            ident: "postcss",
+            config: false,
+            plugins: !useTailwind
+              ? [
+                  "postcss-flexbugs-fixes",
+                  [
+                    "postcss-preset-env",
+                    {
+                      autoprefixer: {
+                        flexbox: "no-2009",
+                      },
+                      stage: 3,
+                    },
+                  ],
+                  // Adds PostCSS Normalize as the reset css with default options,
+                  // so that it honors browserslist config in package.json
+                  // which in turn let's users customize the target behavior as per their needs.
+                  "postcss-normalize",
+                ]
+              : [
+                  "tailwindcss",
+                  "postcss-flexbugs-fixes",
+                  [
+                    "postcss-preset-env",
+                    {
+                      autoprefixer: {
+                        flexbox: "no-2009",
+                      },
+                      stage: 3,
+                    },
+                  ],
+                ],
+          },
+          sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment,
+        },
+      },
+    ].filter(Boolean);
+    if (preProcessor) {
+      loaders.push(...preProcessor);
+    }
+    return loaders;
+  };
+
   return {
     target: ["browserslist"],
     // Webpack noise constrained to errors and warnings
@@ -551,17 +618,36 @@ module.exports = function (webpackEnv) {
             {
               test: lessRegex,
               exclude: lessModuleRegex,
-              use: getStyleLoaders(
+              use: getLessLoaders(
                 {
                   importLoaders: 3,
                   sourceMap: isEnvProduction
                     ? shouldUseSourceMap
                     : isEnvDevelopment,
-                  modules: {
-                    mode: "icss",
-                  },
+                  // modules: {
+                  //   mode: "local",
+                  //   getLocalIdent: getCSSModuleLocalIdent,
+                  // },
                 },
-                "less-loader"
+                [
+                  {
+                    loader: require.resolve("resolve-url-loader"),
+                    options: {
+                      sourceMap: isEnvProduction
+                        ? shouldUseSourceMap
+                        : isEnvDevelopment,
+                      root: paths.appSrc,
+                    },
+                  },
+                  {
+                    loader: require.resolve("less-loader"),
+                    options: {
+                      lessOptions: {
+                        javascriptEnabled: true,
+                      },
+                    },
+                  },
+                ]
               ),
             },
             // Adds support for CSS Modules, but using less
